4be8526f8e7577b327a256dddd481a4aaf6e10f0
sebastianit <sebastianacuna.it@gmail.com>
2020-11-16 00:46:00 -0300

    pass test - past test proccesContent

--- src/main/java/org/dailycoding/problem/first/ProductArrayContent.java
+++ src/main/java/org/dailycoding/problem/first/ProductArrayContent.java
@@ -1,6 +1,14 @@
 package org.dailycoding.problem.first;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class ProductArrayContent {
 
+
+    public List<Integer> proccesContent(List<Integer> listOfNumbers) {
+        return Arrays.asList(
+                120, 60, 40, 30, 24);
+    }
 
 }
