8bd8d84c034b15cf70198d03de031c6a2b66c0d1
sebastianit <sebastianacuna.it@gmail.com>
2020-11-16 00:53:32 -0300

    pass test - past test proccesContent

--- src/main/java/org/dailycoding/problem/first/ProductArrayContent.java
+++ src/main/java/org/dailycoding/problem/first/ProductArrayContent.java
@@ -7,8 +7,10 @@ public final class ProductArrayContent {
 
 
     public List<Integer> proccesContent(List<Integer> listOfNumbers) {
+        System.out.println(listOfNumbers);
         return Arrays.asList(
                 120, 60, 40, 30, 24);
     }
+
 
 }
--- src/test/java/org/dailycoding/problem/test/first/ProductArrayContentTest.java
+++ src/test/java/org/dailycoding/problem/test/first/ProductArrayContentTest.java
@@ -1,20 +1,20 @@
 package org.dailycoding.problem.test.first;
 
 import org.dailycoding.problem.first.ProductArrayContent;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 
 
 public class ProductArrayContentTest {
 
     ProductArrayContent productArrayContent;
 
-    @Before
+    @BeforeEach
     public void setup() throws Exception {
         productArrayContent = new ProductArrayContent();
     }
@@ -24,7 +24,7 @@ public class ProductArrayContentTest {
     public void proccesContent_shouldReturnAnArray() {
         List<Integer> listOfNumbers = productArrayContent.proccesContent(Arrays.asList(
                 1, 2, 3, 4, 5));
-        assertEquals(5, listOfNumbers.size(),0.0);
+        assertEquals(5, listOfNumbers.size(), 0.0);
     }
 
 }
