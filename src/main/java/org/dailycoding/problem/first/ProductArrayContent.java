35086cae83d66837112ce01fe802071718f60a80
sebastianit <sebastianacuna.it@gmail.com>
2020-11-16 01:51:52 -0300

    refactor - refactor getLisWithOutIndex also added other 3 test to ensure code quality

--- src/main/java/org/dailycoding/problem/first/ProductArrayContent.java
+++ src/main/java/org/dailycoding/problem/first/ProductArrayContent.java
@@ -1,9 +1,7 @@
 package org.dailycoding.problem.first;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public final class ProductArrayContent {
 
@@ -13,8 +11,9 @@ public final class ProductArrayContent {
         return listOfProducts;
     }
 
-    public List<Integer> getLisWithOutIndex(List<Integer> listOfNumbers, int numberToFilter) {
-        return Arrays.asList(1, 3, 4, 5);
+    public List<Integer> getLisWithOutIndex(List<Integer> listOfNumbers, int index) {
+        listOfNumbers.remove(index);
+        return new ArrayList(listOfNumbers);
     }
 
 
--- src/test/java/org/dailycoding/problem/test/first/ProductArrayContentTest.java
+++ src/test/java/org/dailycoding/problem/test/first/ProductArrayContentTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 public class ProductArrayContentTest {
@@ -24,14 +27,36 @@ public class ProductArrayContentTest {
     public void proccesContent_shouldReturnAnArray() {
         List<Integer> listOfNumbers = productArrayContent.proccesContent(Arrays.asList(
                 1, 2, 3, 4, 5));
+
         assertEquals(5, listOfNumbers.size(), 0.0);
     }
 
     @Test
-    public void getLisWithOutIndex_shouldReturnListWithOutNumber() {
-        List<Integer> listOfNumbers = productArrayContent.getLisWithOutIndex(Arrays.asList(
-                1, 2, 3, 4, 5) , 2);
+    public void getLisWithOutIndex_shouldReturnListWithOutIndexTwo() {
+        List<Integer> listOfNumbers = productArrayContent.getLisWithOutIndex(new ArrayList(Arrays.asList(
+                1, 2, 3, 4, 5)) , 2);
+
         assertEquals(4, listOfNumbers.size(), 0.0);
+    }
+
+    @Test
+    public void getLisWithOutIndex_shouldReturnListWithOutIndexOne() {
+        List<Integer> listOfNumbers = productArrayContent.getLisWithOutIndex(new ArrayList(Arrays.asList(
+                1, 2, 3, 4, 5)) , 1);
+
+        assertEquals(4, listOfNumbers.size(), 0.0);
+    }
+
+    @Test
+    public void getLisWithOutIndex_shouldReturnListWithOutIndexZero() {
+        List<Integer> testNumbers = new ArrayList(Arrays.asList(
+                1, 2, 3, 4, 5));
+
+        List<Integer> listOfNumbers = productArrayContent.getLisWithOutIndex(new ArrayList(testNumbers) , 0);
+        Integer numberToFind = testNumbers.get(0);
+        Optional<Integer> numberToFilter = listOfNumbers.parallelStream().filter( number -> number.equals(numberToFind)).findFirst();
+
+        assertEquals(numberToFilter.isPresent(), false);
     }
 
 
